182644d914e235b4a060d0ecac82b2221c630c49
WJ Boshoff <wjbosdev@gmail.com>
2025-02-24 14:04:08 +0200

    Leetcode Q20, is Valid Parentheses

--- src/DataStructures/UseCase/stacks/ParenthesesChecking.java
+++ src/DataStructures/UseCase/stacks/ParenthesesChecking.java
@@ -2,7 +2,7 @@ package DataStructures.UseCase.stacks;
 
 import java.util.*;
 
-public class ParenthesisChecking {
+public class ParenthesesChecking {
 
     public static void main(String[] args) {
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import DataStructures.UseCase.stacks.ParenthesisChecking;
-
 public class Main {
     public static void main(String[] args) {
 
